eedd532d5fda1a198a85debaa5742978facd5608
yihang.li@56qq.com <yihang.li@56qq.com>
2019-07-18 15:38:35 +0800

    chapter 01 code

---
+++ chapter01/c01.c
@@ -0,0 +1,12 @@
+#include "c01.h"
+#include <stdio.h>
+//
+// Created by yihang.li on 7/18/2019.
+//
+int compare(int a, int b){
+    int c;
+    if ( a > b ) c = a;
+    else c = b;
+    return c;
+}
+
---
+++ chapter01/c01.h
@@ -0,0 +1,8 @@
+//
+// Created by yihang.li on 7/18/2019.
+//
+
+#ifndef C_PROGRAM_DESIGN_C01_H
+#define C_PROGRAM_DESIGN_C01_H
+extern int compare();
+#endif //C_PROGRAM_DESIGN_C01_H
